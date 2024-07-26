3ee3ae15b4c78fe92701e96ab45d8b78f6a71437
Amali Osadi <kaosadi17@gmail.com>
2024-07-26 15:53:43 +0300

    add custom layout

---
+++ layouts/product.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header>
+            <h1>product</h1>
+        </header>  
+        <div>
+            <slot/>
+        </div>
+        <footer>
+            <nav>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink> </li>
+                <li><NuxtLink to="/about">About</NuxtLink> </li>
+            </ul>
+        </nav>
+        </footer>  
+    </div>
+</template>
