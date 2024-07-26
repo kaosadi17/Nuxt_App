81104d858f6845db6c271bdacf2fb6e3a5c5773f
Amali Osadi <kaosadi17@gmail.com>
2024-07-26 15:54:26 +0300

    use custom layout

---
+++ pages/products/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>Products</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore magnam pariatur consectetur consequatur! At
+            ipsum mollitia delectus, quae error quidem? Voluptates nesciunt distinctio iusto eius praesentium deleniti
+            enim facilis non!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore saepe ad reprehenderit laboriosam in quis
+            atque tempore, eius nam, at explicabo incidunt maxime repudiandae voluptas, deserunt dignissimos ullam vero
+            neque.</p>
+    </div>
+</template>
+
+<script setup>
+    definePageMeta({
+        layout: 'product'
+    })
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
