dd13d3e56a90708fcf8126039f681dd5e482fd21
Amali Osadi <kaosadi17@gmail.com>
2024-07-29 16:16:07 +0300

    create ProductCard component

---
+++ components/ProductCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img :src="product.image" alt="product img">
+        <p>{{ product.title }}</p>
+        <NuxtLink :to="`/products/${product.id}`">View Details</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+    const { product } = defineProps(['product'])
+</script>
+
+<style scoped>
+    img{
+        width: 100px;
+        height: 100px;
+    }
+</style>
