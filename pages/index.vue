66bcf8f00030644f779ad874d900d29e384677a9
Amali Osadi <kaosadi17@gmail.com>
2024-07-29 11:50:27 +0300

    Fetch products data from API

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,23 @@
 <template>
     <div>
-        <h2>Home</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore magnam pariatur consectetur consequatur! At
-            ipsum mollitia delectus, quae error quidem? Voluptates nesciunt distinctio iusto eius praesentium deleniti
-            enim facilis non!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore saepe ad reprehenderit laboriosam in quis
-            atque tempore, eius nam, at explicabo incidunt maxime repudiandae voluptas, deserunt dignissimos ullam vero
-            neque.</p>
+        <h2>Products</h2>
+        <div v-for="p in products" :key="p.id">
+            <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+        </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+    interface Product {
+        id: number;
+        title: string
+    }
 
+    definePageMeta({
+        layout: 'product'
+    });
+
+    const { data: products } = await useFetch<Product[]>('https://fakestoreapi.com/products');
 </script>
 
 <style scoped>
