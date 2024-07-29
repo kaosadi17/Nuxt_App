6f4439a9b422de7fa7a18122a109aa2eaec4df2c
Amali Osadi <kaosadi17@gmail.com>
2024-07-29 14:43:23 +0300

    fetch single product data

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,11 +1,23 @@
 <template>
-    <h2> Product details for {{ id }}</h2>
+    <div>
+        <h2> Product details for {{ id }}</h2>
+        <p>{{ product.title }}</p>
+    </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
     const { id } = useRoute().params
+    const url = "https://fakestoreapi.com/products/" + id
+    const {data: product} = await useFetch(url);
+
+    definePageMeta({
+        layout: 'product'
+    })
+    
+
 </script>
 
 <style scoped>
 
 </style>
+
