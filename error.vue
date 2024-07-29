ac06a005552fa95087b682f1df272122d3ac3745
Amali Osadi <kaosadi17@gmail.com>
2024-07-30 00:31:22 +0300

    add error handling page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>{{ error.statusCode }}</p>
+        <p>{{ error.message }}</p>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['error'])
+</script>
+
+<style scoped>
+
+</style>
